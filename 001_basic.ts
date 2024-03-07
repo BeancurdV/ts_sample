39b6ce293cba61dcf4668eafbc649d55abe5c980
BeancurdV <beancurdv0@gmail.com>
2024-03-07 15:13:18 +0800

    [done]

--- 001_basic.ts
+++ 001_basic.ts
@@ -1,3 +1,22 @@
-let obj = {name:'zfc'}
 
+// 最简单的打印
+let obj = {name:'zfc'}
 console.log(obj.name)
+
+
+// structal inherient 
+interface Living {
+    name:string , 
+    age: number
+}
+
+function introduce(some: Living) {
+    console.log("name : %s , age : %s" , some.name , some.age)
+}
+
+const person = {
+    name : 'beancurdv' , 
+    age : 33
+}
+
+introduce(person)
