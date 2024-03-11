064abd917dbc58a47d1130c638899df377a17e18
BeancurdV <beancurdv0@gmail.com>
2024-03-11 16:56:34 +0800

    [基本语法] 一

--- 001_basic.ts
+++ 001_basic.ts
@@ -1,22 +1,72 @@
+/* 案例一 */
+// let message = "hello world";
+// message();
 
-// 最简单的打印
-let obj = {name:'zfc'}
-console.log(obj.name)
+/* 案例二 */
+
+// const user = {
+//     name: "Daniel",
+//     age : 26
+// }
+// // 访问不存在的属性
+// console.log(user.location)
 
 
-// structal inherient 
-interface Living {
-    name:string , 
-    age: number
-}
+/* 案例三 */
 
-function introduce(some: Living) {
-    console.log("name : %s , age : %s" , some.name , some.age)
-}
+// import express , {Express ,Request, Response } from "express"
 
-const person = {
-    name : 'beancurdv' , 
-    age : 33
-}
+// const app : Express = express()
+// app.get("/", function(req,res){
+//     res.send
+// })
 
-introduce(person)
+// let big : bigint = BigInt("1000");
+
+// let sym : symbol = Symbol("LIN");
+
+// let num :any = 0 ; 
+// num = true ; 
+
+// function getCount(NUM:unknown) {
+//     return (NUM as number) / 2;
+// }
+
+// const ob =  {
+//     name : 'aaa',
+//     age:1
+// }
+
+
+// console.log(ob)
+
+// // TODO zfc 如何扩展属性呢？
+// ob.age = 11
+
+
+// // 函数
+// let add2 = (x:number , y : number) => x+y;
+// let add3 = (x:number , y : number) => {
+//     return x + y;
+// }
+
+
+// interface Animail {
+//     name:String , 
+//     age?:number
+// }
+
+// const dog:Animail = {
+//     name : "dog",
+//     age : 2
+// }
+
+// console.log(typeof dog)
+
+
+// interface showArrays {
+//     [key:n umber]:number
+// }
+
+// const arr:showArrays  = [1,2,3]
+
---
+++ 001_basic_.js
@@ -0,0 +1,13 @@
+// 案例一
+
+// let message = "hello world";
+// message();
+
+
+// 案例二
+const user = {
+        name: "Daniel",
+        age: 26
+    }
+    // 访问不存在的属性, 返回undefined
+console.log(user.location)
